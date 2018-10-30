00d83dfbf2119e08230ee2d2ed23964e2228ee6d
Ryan Carver <ryan@ryancarver.com>
2018-10-30 14:49:42 -0700

    Searcing gallery

---
+++ src/pages/searching.php
@@ -0,0 +1,108 @@
+<?php include "_variables.php" ?>
+<?php include "_media_queries.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ryan Carver Photography | Searching</title>
+
+    <?php include "_assets.php" ?>
+  </head>
+  <body>
+    <?php include "_menu.php" ?>
+    <?php include "_header.php" ?>   
+    
+    <div class="rc-gallery-header-container">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/header.jpg"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/header.jpg 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/header.jpg 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/header.jpg 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/header.jpg 480w"
+              sizes="<?= $gallery_sizes_header ?>"
+class="v">
+      <div class="rc-content">
+        <p class="rc-intro">
+          <span class="rc-title">Searching</span> 
+          <span class="rc-subtitle">Images 2013-2018. Arranged 2018.</span>
+        </p>
+        <p class="rc-copy">
+          Searching is a story of loss and the struggle to replace it.
+        </p>
+        <p class="rc-copy">
+Repeating images of interiors, exteriors, roads, and water create the circular
+logic of one’s mind when attempting to find answers.  The sequencing and
+placement of each image forms a visual narrative, through which the viewer is
+invited to participate in their own search for what is lost.  
+        </p>
+        <p class="rc-copy">
+The images in this series were captured between 2013 and 2018, in
+moments between life. The loss, and the purpose of the search are as yet
+unknown to the artist. 
+        </p>
+        <p class="rc-copy">
+Presented as 18 giclée prints on Hahnemühle Photo Rag. Large images are 30x40 inches, small are 15x20 inches.
+        </p>
+      </div>
+    </div>
+
+    <div class="rc-gallery-container rc-searching-gallery-container">
+      <div class="row">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-01.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-01.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-01.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-01.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-01.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-02.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-02.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-02.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-02.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-02.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-03.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-03.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-03.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-03.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-03.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-04.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-04.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-04.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-04.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-04.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-05.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-05.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-05.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-05.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-05.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-06.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-06.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-06.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-06.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-06.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-07.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-07.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-07.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-07.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-07.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+        <img    src="<?= $baseurl ?>/img/gallery/sm/searching/screen-08.png"
+             srcset="<?= $baseurl ?>/img/gallery/lg/searching/screen-08.png 1280w, 
+                     <?= $baseurl ?>/img/gallery/md/searching/screen-08.png 1000w, 
+                     <?= $baseurl ?>/img/gallery/sm/searching/screen-08.png 800w, 
+                     <?= $baseurl ?>/img/gallery/xs/searching/screen-08.png 480w"
+              sizes="<?= $gallery_sizes_1up ?>">
+      </div>
+    </div>
+
+    <?php include "_backtotop.php" ?>
+    <?php include "_footer.php" ?> 
+
+    <?php include "_scripts.php" ?> 
+  </body>
+</html>
