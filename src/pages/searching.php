356cecebaf00b9cec8ac3b1fe5d3f4e2b40dacb1
Ryan Carver <ryan@ryancarver.com>
2018-11-05 14:19:08 -0800

    copy edits: searching

--- src/pages/searching.php
+++ src/pages/searching.php
@@ -28,16 +28,21 @@
           <span class="rc-subtitle">2018</span>
         </p>
         <p class="rc-copy">
-Sometimes what’s lost is not so much a thing, but a feeling. <br>Perhaps an opportunity. Opportunity cost, they call it. 
+Sometimes what’s lost is not so much a thing, but a feeling. <br>Perhaps an
+opportunity. Opportunity cost, they call it. 
         </p>
         <p class="rc-copy">
-This series explores the struggle that is knowing there’s something else out there.  Repeating images of interiors, exteriors, roads, and water create the circular logic of a search for answers. The sequencing and placement of each image forms a visual narrative through which I invite you to explore your own opportunity lost.
+This series explores the struggle of knowing there’s something else out there.
+Repeating images of interiors, exteriors, roads, and water create the circular
+logic in a search for answers. The sequencing and placement of each image form
+a visual narrative through which I invite you to consider your own opportunity
+lost.
         </p>
         <p class="rc-copy">
 The images in this series were captured between 2013 and 2018, in the
 in-between moments of my life. For years I’ve found myself drawn to these empty
-scenes; I use the camera to capture the feeling of wonder of what could have been, or could still
-be.
+scenes; I use the camera to capture the feeling of wonder of what could have
+been, or could still be.
         </p>
         <p class="rc-copy">
 <i>Lost</i> is presented as 18 giclée prints on Hahnemühle Photo Rag. Largest images are 30x40 inches, smallest are 15x20 inches.
